8ddddaf96ee859e7dd518d8bfc67e36e5d9b9d70
kuktaekim <ktkim@slac.stanford.edu>
2024-05-07 09:54:01 -0700

    clean up the pydm directory.

--- pydm/atca-llrf-demo-ui.bash
+++ pydm/atca-llrf-demo-ui.bash
@@ -5,7 +5,7 @@
 
 pydm --version
 
-PYDM_STYLESHEET_INCLUDE_DEFAULT=1 pydm -m "LOCA=B084,UNIT=RF70,INST=0,TCAV_LOCA=B084,TCAV_UNIT=RF70,IOC_UNIT=RF70, DESC=NC,SIOC=SIOC:B084:RF70,DEV=TCAV:B084:RF70:0" \
+PYDM_STYLESHEET_INCLUDE_DEFAULT=1 pydm -m "LOCA=B084,UNIT=RF70,INST=0,TCAV_LOCA=B084,TCAV_UNIT=RF70,IOC_UNIT=RF70, DESC=DEMO,SIOC=SIOC:B084:RF70,DEV=TCAV:B084:RF70:0" \
     --stylesheet $TOOLS/pydm/stylesheet/default.qss \
     --stylesheet $PYDM/klys/llrf.qss \
-    /afs/slac/g/controls/development/users/ktkim/sandbox/llrfATCA/pydm/xtcav_atcaLLRF_pau.py &
+    /afs/slac/g/controls/development/users/ktkim/sandbox/llrfATCA/pydm/atcaLLRF_demo.py &
